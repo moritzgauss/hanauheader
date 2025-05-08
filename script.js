ccb996f8e8476e2a8b8d870e491a38167ff7dfa0
moritzgauss <moritzgg99@gmail.com>
2025-05-08 12:27:43 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ const counterEl = document.getElementById("day-counter");
 const digits = diffDays.toString().padStart(5, "0");
 
 counterEl.innerHTML = digits.split('').map(d => `<span>${d}</span>`).join('');
+
+// Add event listeners for mobile click functionality
+const cards = document.querySelectorAll('.card');
+
+cards.forEach(card => {
+  card.addEventListener('click', () => {
+    // Remove 'is-flipped' class from other cards
+    cards.forEach(c => {
+      if (c !== card) {
+        c.classList.remove('is-flipped');
+      }
+    });
+
+    // Toggle 'is-flipped' class on the clicked card
+    card.classList.toggle('is-flipped');
+  });
+});
