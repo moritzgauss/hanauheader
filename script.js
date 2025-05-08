b33e1f0423f9403a69f75accb68b2421dbc9e5fd
moritzgauss <moritzgg99@gmail.com>
2025-05-08 12:55:51 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const today = new Date();
 const diffTime = today - startDate;
 const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
 const counterEl = document.getElementById("day-counter");
-const digits = diffDays.toString().padStart(5, "0");
+const digits = diffDays.toString().padStart(4, "0");
 
 counterEl.innerHTML = digits.split('').map(d => `<span>${d}</span>`).join('');
 
